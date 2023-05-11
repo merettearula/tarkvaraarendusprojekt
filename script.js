f2407f9235f1661c979d5585835d1ad10b50439d
Merette Arula <114983020+merettearula@users.noreply.github.com>
2023-05-11 11:28:29 +0300

    Merette tagasiside + stat

--- script.js
+++ script.js
@@ -6,35 +6,42 @@ var sortDirection = {
   function filterResults() {
     var fromInput = document.getElementById('from');
     var toInput = document.getElementById('to');
-    var fromDate = parseDate(fromInput.value);
-    var toDate = parseDate(toInput.value);
-    
+    var fromDate = new Date(fromInput.value);
+    var toDate = new Date(toInput.value);
+  
     var table = document.getElementById('resultsTable');
     var rows = table.getElementsByTagName('tr');
-    
+  
     for (var i = 1; i < rows.length; i++) {
       var row = rows[i];
-      var dateText = row.cells[4].textContent;
-      var rowDate = parseDate(dateText);
-    
-      if (fromDate && rowDate < fromDate) {
-        row.style.display = 'none';
-      } else if (toDate && rowDate > toDate) {
-        row.style.display = 'none';
-      } else {
-        row.style.display = '';
+      var dateCell = row.cells[0]; // Assuming the date column is at index 0
+  
+      if (dateCell) {
+        var dateText = dateCell.textContent;
+        var rowDate = new Date(dateText);
+  
+        if (fromDate && rowDate < fromDate) {
+          row.style.display = 'none';
+        } else if (toDate && rowDate > toDate) {
+          row.style.display = 'none';
+        } else {
+          row.style.display = '';
+        }
       }
     }
   }
   
+  
+  
+  
+  
   function parseDate(dateString) {
-    var parts = dateString.split('.');
-    var day = parseInt(parts[0], 10);
-    var month = parseInt(parts[1], 10);
-    var year = parseInt(parts[2], 10);
+    var parts = dateString.split('-'); 
+    var day = parseInt(parts[2], 10); // Day is at index 2
+    var month = parseInt(parts[1], 10); // Month is at index 1
+    var year = parseInt(parts[0], 10); // Year is at index 0
     return new Date(year, month - 1, day);
   }
-  
   
 
   function clearFilter() {
@@ -42,8 +49,8 @@ var sortDirection = {
     var toInput = document.getElementById('to');
     
     // Reset the date inputs
-    fromInput.value = '2023-01-01';
-    toInput.value = '2023-12-31';
+    fromInput.value = 'none';
+    toInput.value = 'none';
     
     filterResults(); // Apply the filter with the cleared values
   }
@@ -144,4 +151,42 @@ filterResults(); // Käivitab algse filtreerimise
 function logout(){
     window.location.href = "http://www.tlu.ee/~merette/tarkvaraarendusprojekt/logi_sisse.html";
 }
+
+function exportToExcel() {
+  var table = document.getElementById('resultsTable');
+
+  // Create a new workbook
+  var workbook = XLSX.utils.book_new();
+
+  // Convert the table to a worksheet
+  var worksheet = XLSX.utils.table_to_sheet(table);
+
+  // Add the worksheet to the workbook
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+  // Generate an Excel file
+  var excelFile = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+
+  // Save the file
+  saveAsExcelFile(excelFile, 'table.xlsx');
+}
+
+function saveAsExcelFile(data, filename) {
+  var blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+  var url = URL.createObjectURL(blob);
+
+  // Create a download link
+  var a = document.createElement('a');
+  a.href = url;
+  a.download = filename;
+
+  // Append the link to the document and trigger the download
+  document.body.appendChild(a);
+  a.click();
+
+  // Clean up
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
+
   
